9bf567befbd127769c6e0cbc46a48c236876abf9
wtlin1228 <wtlin1228@gmail.com>
2024-06-16 02:47:27 +0800

    Filesystem isolation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use anyhow::Context;
-use std::io::copy;
+use std::process::Stdio;
+use tempfile::tempdir;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> anyhow::Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    let temp_dir = tempdir().context("create tempdir")?;
+
+    // create /dev/null file
+    let path_dev_null = temp_dir.path().join("dev/null");
+    std::fs::create_dir_all(path_dev_null.parent().unwrap()).context("create dir /dev")?;
+    std::fs::File::create(path_dev_null).context("create file /dev/null")?;
+
+    // copy command
+    let path_command = temp_dir
+        .path()
+        .join(command.strip_prefix("/").unwrap_or(command));
+    std::fs::create_dir_all(path_command.parent().unwrap())
+        .context(format!("create dir {:?}", path_command.parent().unwrap()))?;
+    std::fs::copy(command, path_command).context("copy command")?;
+
+    // chroot jail
+    std::os::unix::fs::chroot(temp_dir.path()).context("chroot into temporary directory")?;
+
+    let status = std::process::Command::new(command)
         .args(command_args)
-        .output()
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .status()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
@@ -16,18 +38,8 @@ fn main() -> anyhow::Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let mut process_stdout = &output.stdout[..];
-        let mut stdout = std::io::stdout().lock();
-        copy(&mut process_stdout, &mut stdout)
-            .context("pipe process's stdout into parent's stdout")?;
-
-        let mut process_stderr = &output.stderr[..];
-        let mut stderr = std::io::stderr().lock();
-        copy(&mut process_stderr, &mut stderr)
-            .context("pipe process's stderr into parent's stderr")?;
-    } else {
-        std::process::exit(output.status.code().unwrap_or(1));
+    if let Some(code) = status.code() {
+        std::process::exit(code);
     }
 
     Ok(())
