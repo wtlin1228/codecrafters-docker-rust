bce196208ab1fdc26815d8f214bc3786806ace8c
wtlin1228 <wtlin1228@gmail.com>
2024-06-17 01:07:09 +0800

    Fetch an image from the Docker Registry

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,67 @@ use tempfile::tempdir;
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> anyhow::Result<()> {
     let args: Vec<_> = std::env::args().collect();
+    let docker_image = &args[2];
     let command = &args[3];
     let command_args = &args[4..];
 
+    let (image_name, image_tag) = docker_image
+        .split_once(':')
+        .unwrap_or((docker_image, "latest"));
+
+    let client = reqwest::blocking::Client::new();
+
+    // requesting a token
+    let token_requesting_url = format!(
+        "https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{}:pull",
+        image_name
+    );
+    let v = client
+        .get(&token_requesting_url)
+        .send()
+        .context(format!("GET {}", token_requesting_url))?
+        .json::<serde_json::Value>()
+        .context("deserialize response to json")?;
+    let token = v["token"].as_str().context("get token")?;
+
+    // fetch the image manifests
+    let image_manifest_url = format!(
+        "https://registry.hub.docker.com/v2/library/{}/manifests/{}",
+        image_name, image_tag
+    );
+    let v = client
+        .get(&image_manifest_url)
+        .header(
+            "Accept",
+            "application/vnd.docker.distribution.manifest.v2+json",
+        )
+        .bearer_auth(token)
+        .send()
+        .context(format!("GET {}", image_manifest_url))?
+        .json::<serde_json::Value>()
+        .context("deserialize response to json")?;
+    let layers = v["layers"].as_array().context("get layers")?;
+
     let temp_dir = tempdir().context("create tempdir")?;
+
+    // pull layers
+    for layer in layers {
+        let digest = layer["digest"].as_str().context("get digest")?;
+        let image_layer_url = format!(
+            "https://registry.hub.docker.com/v2/library/{}/blobs/{}",
+            image_name, digest
+        );
+        let blob = client
+            .get(&image_layer_url)
+            .bearer_auth(token)
+            .send()
+            .context(format!("GET {}", image_layer_url))?
+            .bytes()
+            .context("get bytes")?;
+        tar::Archive::new(flate2::read::GzDecoder::new(&blob[..]))
+            .unpack(temp_dir.path())
+            .context(format!("unpack layer {}", digest))?;
+    }
 
     // create /dev/null file
     let path_dev_null = temp_dir.path().join("dev/null");
